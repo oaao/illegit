f04570588ccf9ce377714e9ebbb0896aec85fec3
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-08-05 14:21:52 -0600

    rev [cli]: use naked methods instead of pointless class

--- illegit/cli/parser.py
+++ illegit/cli/parser.py
@@ -1,23 +1,23 @@
 import argparse
 
-from .commands import Cmd
+from .commands import *
 
 # to be called in illegit mainfile with COMMANDS[args.command](args)
 COMMANDS = {
-    'add':         Cmd.add,
-    'cat-file':    Cmd.cat_file,
-    'checkout':    Cmd.checkout,
-    'commit':      Cmd.commit,
-    'hash-object': Cmd.hash_object,
-    'init':        Cmd.init,
-    'log':         Cmd.log,
-    'ls-tree':     Cmd.ls_tree,
-    'merge':       Cmd.merge,
-    'rebase':      Cmd.rebase,
-    'rev-parse':   Cmd.rev_parse,
-    'rm':          Cmd.rm,
-    'show-ref':    Cmd.show_ref,
-    'tag':         Cmd.tag
+    'add':         add,
+    'cat-file':    cat_file,
+    'checkout':    checkout,
+    'commit':      commit,
+    'hash-object': hash_object,
+    'init':        init,
+    'log':         log,
+    'ls-tree':     ls_tree,
+    'merge':       merge,
+    'rebase':      rebase,
+    'rev-parse':   rev_parse,
+    'rm':          rm,
+    'show-ref':    show_ref,
+    'tag':         tag
 }
 
 p     = argparse.ArgumentParser(description='illegit command processor')
